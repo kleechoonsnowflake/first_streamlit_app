650ded6518198d8dfd4e215a9d7b4b6232bbffb2
kleechoonsnowflake <125102714+kleechoonsnowflake@users.noreply.github.com>
2023-02-22 20:38:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit 
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
