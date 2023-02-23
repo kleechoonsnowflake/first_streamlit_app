e87809389cf1efcb1285c267e71b7c1a16616487
kleechoonsnowflake <125102714+kleechoonsnowflake@users.noreply.github.com>
2023-02-22 20:35:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalize response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put in table
+streamlit.dataframe(fruityvice_normalized)
+
